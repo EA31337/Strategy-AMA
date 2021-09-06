7d5d136109a7ffe20ae4fe512c7f8f7d1bd77ee0
kenorb <kenorb@users.noreply.github.com>
2021-09-06 20:24:08 +0100

    Converts Demo to AMA strategy

--- config/H4.h
+++ config/H4.h
@@ -4,12 +4,12 @@
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
-struct Indi_Demo_Params_H4 : DemoIndiParams {
-  Indi_Demo_Params_H4() : DemoIndiParams(indi_demo_defaults, PERIOD_H4) { shift = 0; }
-} indi_demo_h4;
+struct Indi_AMA_Params_H4 : IndiAMAParams {
+  Indi_AMA_Params_H4() : IndiAMAParams(indi_ama_defaults, PERIOD_H4) { shift = 0; }
+} indi_ama_h4;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
-struct Stg_Demo_Params_H4 : StgParams {
+struct Stg_AMA_Params_H4 : StgParams {
   // Struct constructor.
-  Stg_Demo_Params_H4() : StgParams(stg_demo_defaults) {}
-} stg_demo_h4;
+  Stg_AMA_Params_H4() : StgParams(stg_ama_defaults) {}
+} stg_ama_h4;
--- config/M30.h
+++ config/M30.h
@@ -4,12 +4,12 @@
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
-struct Indi_Demo_Params_M30 : DemoIndiParams {
-  Indi_Demo_Params_M30() : DemoIndiParams(indi_demo_defaults, PERIOD_M30) { shift = 0; }
-} indi_demo_m30;
+struct Indi_AMA_Params_M30 : IndiAMAParams {
+  Indi_AMA_Params_M30() : IndiAMAParams(indi_ama_defaults, PERIOD_M30) { shift = 0; }
+} indi_ama_m30;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
-struct Stg_Demo_Params_M30 : StgParams {
+struct Stg_AMA_Params_M30 : StgParams {
   // Struct constructor.
-  Stg_Demo_Params_M30() : StgParams(stg_demo_defaults) {}
-} stg_demo_m30;
+  Stg_AMA_Params_M30() : StgParams(stg_ama_defaults) {}
+} stg_ama_m30;
